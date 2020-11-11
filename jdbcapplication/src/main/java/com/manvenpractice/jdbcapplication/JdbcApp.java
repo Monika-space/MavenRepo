b6b88e4724305304ae03369768a40c1a06c138e1
Monika-space <73583258+Monika-space@users.noreply.github.com>
2020-11-11 19:15:37 +0530

    adding JDBC application to remote repo

--- jdbcapplication/src/main/java/com/manvenpractice/jdbcapplication/JdbcApp.java
+++ jdbcapplication/src/main/java/com/manvenpractice/jdbcapplication/JdbcApp.java
@@ -1,14 +1,22 @@
 package com.manvenpractice.jdbcapplication;
 import java.sql.*;
-public class JdbcApp {
+public class JdbcApp 
+{
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
        Class.forName("oracle.jdbc.OracleDriver");
-       Connection con = DriverManager.getConnection(url, user, password);
+       Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","monika","luckky16");
        Statement st = con.createStatement();
-       ResultSet rs = st.executeQuery("select * from Employee");
+       if(con==null)
+       {
+    	   System.out.println("not connected to oracle DB");
+       }
+       else
+       {
+    	   System.out.println("connected");
+       }
+       //ResultSet rs = st.executeQuery("select * from Employee");
        con.close();
 	}
-
 }
