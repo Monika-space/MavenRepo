207611f8c8d14515be8d26c281e2826c0515067a
Monika-space <73583258+Monika-space@users.noreply.github.com>
2020-12-04 15:14:12 +0530

    My first spring application

---
+++ SpringApplication/src/main/java/com/springpractice/SpringApplication/App.java
@@ -0,0 +1,13 @@
+package com.springpractice.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
